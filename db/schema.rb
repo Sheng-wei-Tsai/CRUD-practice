d583c5238927977a62683f96176d3af85cbc6380
Henry.Tsai <henry88002605@gmail.com>
2020-04-10 07:19:43 +0800

    generate vote_logs_count:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_144940) do
+ActiveRecord::Schema.define(version: 2020_04_09_214900) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2020_04_08_144940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "vote_logs", force: :cascade do |t|
+    t.integer "candidate_id", null: false
+    t.string "ip_address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["candidate_id"], name: "index_vote_logs_on_candidate_id"
+  end
+
+  add_foreign_key "vote_logs", "candidates"
 end
