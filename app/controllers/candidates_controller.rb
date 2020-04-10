9218de5bcfb0c0978cc10ab6cacc57dce56ae3b2
Henry.Tsai <henry88002605@gmail.com>
2020-04-10 08:57:04 +0800

    install bootrsap4 Sprockets Rails gem remove all of words in asset application scss

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,5 @@
 class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:edit, :update, :vote, :destroy]
   def index
     @candidates = Candidate.all
   end
@@ -17,11 +18,9 @@ class CandidatesController < ApplicationController
   end
 
   def edit
-    @candidate = Candidate.find(params[:id])
   end
 
   def update
-    @candidate = Candidate.find(params[:id])
     if @candidate.update(candidate_params)
       redirect_to candidates_path , notice: "資料更新成功!"
     else
@@ -30,20 +29,24 @@ class CandidatesController < ApplicationController
   end
 
   def vote
-    @candidate = Candidate.find(params[:id])
     @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
     redirect_to candidates_path, notice: "完成投票!"
   end
 
 
   def destroy
-    @candiate = Candidate.find(params[:id])
     @candiate.destroy if @candiate 
     redirect_to candidates_path, notice: "刪除成功!!"
   end
+
 
   private 
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
   end
+
+  def find_candidate
+    @candiate = Candidate.find(params[:id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_214900) do
+ActiveRecord::Schema.define(version: 2020_04_09_232219) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_214900) do
     t.integer "votes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vote_logs_count"
   end
 
   create_table "vote_logs", force: :cascade do |t|
