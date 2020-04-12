869e5fe9e18a211436b99cdeb2e74ff404065853
Henry.Tsai <henry88002605@gmail.com>
2020-04-12 15:02:28 +0800

    add icon_link_to viewhepler application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
-  def user_gender(gender)
-    if gender == 1
-      "boy"
-    elsif gender == 0
-      "girl"
-    else
-      "I don't want to talk"
+  def icon_link_to(label, path, icon)
+    link_to path do
+      content_tag :span, class: "glyphicon glyphicon-#{icon} aria-hidden='true' " do
+        label
+      end 
     end
   end
 end
